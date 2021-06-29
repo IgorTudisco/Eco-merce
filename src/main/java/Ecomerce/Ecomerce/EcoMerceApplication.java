d99a550e8f266a62af45da0c6f8e6b9e517b6ac0
Beatriz Aparecida de Freitas Rodrigues <beeatrizz29@gmail.com>
2021-06-29 09:31:39 -0300

    Criação do projeto

---
+++ src/main/java/Ecomerce/Ecomerce/EcoMerceApplication.java
@@ -0,0 +1,13 @@
+package Ecomerce.Ecomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoMerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoMerceApplication.class, args);
+	}
+
+}
