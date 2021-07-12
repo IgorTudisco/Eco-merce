7e42d840512b89be119b066b5a4fdc452de97127
Gesus-hub <guimaraes.viniciusdasilva@gmail.com>
2021-07-12 10:50:57 -0300

    Sistema de seguranca implementado!

--- src/main/java/Ecomerce/Ecomerce/seguranca/UserDetailsImplements.java
+++ src/main/java/Ecomerce/Ecomerce/seguranca/UserDetailsImplements.java
@@ -1,5 +1,63 @@
 package Ecomerce.Ecomerce.seguranca;
 
-public class UserDetailsImplements {
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import Ecomerce.Ecomerce.model.UsuarioCadastro;
+
+public class UserDetailsImplements implements UserDetails{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String userEmail;
+	
+	private String password;
+
+	public UserDetailsImplements(UsuarioCadastro usuarioCadastro) {
+		super();
+		this.userEmail = usuarioCadastro.getEmail();
+		this.password = usuarioCadastro.getSenha();
+	}
+	
+	public UserDetailsImplements() {
+		super();
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return userEmail;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 
 }
--- src/main/java/Ecomerce/Ecomerce/seguranca/UserDetailsServiceImplement.java
+++ src/main/java/Ecomerce/Ecomerce/seguranca/UserDetailsServiceImplement.java
@@ -1,5 +1,25 @@
 package Ecomerce.Ecomerce.seguranca;
 
-public class UserDetailsServiceImplement {
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import Ecomerce.Ecomerce.model.UsuarioCadastro;
+import Ecomerce.Ecomerce.repository.UsuarioCadastroRepository;
+
+public class UserDetailsServiceImplement implements UserDetailsService{
+
+	@Autowired
+	private UsuarioCadastroRepository cadastroUsuarioRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+		Optional<UsuarioCadastro> cadastro = cadastroUsuarioRepository.findByEmail(userEmail);
+		cadastro.orElseThrow(() -> new UsernameNotFoundException(userEmail + " not found."));
+		return cadastro.map(UserDetailsImplements::new).get();
+	}
 
 }
