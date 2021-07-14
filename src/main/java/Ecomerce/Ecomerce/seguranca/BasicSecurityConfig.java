baa378b096861fee9c742b2ec1d3f7e7f5af1040
Beatriz Aparecida de Freitas Rodrigues <beeatrizz29@gmail.com>
2021-07-14 11:50:18 -0300

    Fazendo atualizações

--- src/main/java/Ecomerce/Ecomerce/seguranca/BasicSecurityConfig.java
+++ src/main/java/Ecomerce/Ecomerce/seguranca/BasicSecurityConfig.java
@@ -11,39 +11,32 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
-	
+public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
+
 	@Autowired
 	private UserDetailsServiceImplement userDetailsService;
-	
-	@Override 
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception { 
-        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("admin")).authorities("ROLE_ADMIN");
-        
-        auth.userDetailsService(userDetailsService);
-    }
-	
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("admin"))
+				.authorities("ROLE_ADMIN");
+
+		auth.userDetailsService(userDetailsService);
+	}
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		
+
 		return new BCryptPasswordEncoder();
-		
+
 	};
-	
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
-		http.authorizeRequests()
-		.antMatchers("/usuarios/logar").permitAll()
-		.antMatchers("/usuarios/cadastrar").permitAll()
-		.anyRequest().authenticated() 
-		.and().httpBasic() 
-		.and().sessionManagement() 
-		.sessionCreationPolicy(SessionCreationPolicy.STATELESS) 
-		.and().cors() 
-		.and().csrf().disable() 
-		;
+
+		http.authorizeRequests().antMatchers("/usuarios/logar").permitAll().antMatchers("/usuarios/cadastrar")
+				.permitAll().anyRequest().authenticated().and().httpBasic().and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and().csrf().disable();
 	}
-	
+
 }
