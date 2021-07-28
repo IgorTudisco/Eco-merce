9986acf8947c52e0df80e96977f3f608eb875fb7
Denise Mignoli <denisemignoli@gmail.com>
2021-07-28 10:49:37 -0300

    Adicionando os componentes base

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { LoginComponent } from './login/login.component';
+import { SobreNosComponent } from './sobreNos/sobreNos.component';
+import { ContatoComponent } from './contato/contato.component';
+import { LoginCadastroComponent } from './loginCadastro/loginCadastro.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [				
+    AppComponent,
+    MenuComponent,
+      RodapeComponent,
+      LoginComponent,
+      SobreNosComponent,
+      ContatoComponent,
+      LoginCadastroComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
