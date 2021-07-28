335f1ae8600c094d819ceb63c324d9b87882e547
Gesus-hub <guimaraes.viniciusdasilva@gmail.com>
2021-07-28 12:30:42 -0300

    Criando Navbar

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
-import { LoginComponent } from './login/login.component';
 import { SobreNosComponent } from './sobreNos/sobreNos.component';
 import { ContatoComponent } from './contato/contato.component';
 import { LoginCadastroComponent } from './loginCadastro/loginCadastro.component';
@@ -15,7 +14,6 @@ import { LoginCadastroComponent } from './loginCadastro/loginCadastro.component'
     AppComponent,
     MenuComponent,
       RodapeComponent,
-      LoginComponent,
       SobreNosComponent,
       ContatoComponent,
       LoginCadastroComponent
