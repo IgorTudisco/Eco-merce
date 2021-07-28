2784697c2975629e274d9e3d991b87fb38440e02
Michele Diones <pereiradionesm@gmail.com>
2021-07-28 15:49:50 -0300

    Implementando componentes e rotas

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
+import { ContatoComponent } from './contato/contato.component';
+import { SobreNosComponent } from './sobreNos/sobreNos.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { MenuComponent } from './menu/menu.component';
+import { LoginCadastroComponent } from './loginCadastro/loginCadastro.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'',
+  redirectTo:'entrar',
+  pathMatch:'full'
+},
+{
+  path:'entrar',
+ component: LoginCadastroComponent
+},
+{
+  path:'menu',
+ component: MenuComponent
+},
+{
+  path:'rodape',
+ component: RodapeComponent
+},
+{
+  path:'sobreNos',
+ component: SobreNosComponent
+},
+{
+  path:'contato',
+ component: ContatoComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
