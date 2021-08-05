9bab57d043620cfacc7dbe2fec712a5b859bc829
Beatriz Aparecida de Freitas Rodrigues <beeatrizz29@gmail.com>
2021-08-05 11:11:49 -0300

    Deletando componente de usuario errado

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -16,7 +16,6 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { CarouselComponent } from './carousel/carousel.component';
 import { Parallax2Component } from './parallax2/parallax2.component';
 import { UsuarioEmpresaComponent } from './usuario-empresa/usuario-empresa.component';
-import { UuarioCooperativaComponent } from './uuario-cooperativa/uuario-cooperativa.component';
 import { UsuarioClienteComponent } from './usuario-cliente/usuario-cliente.component';
 import { VoucherComponent } from './voucher/voucher.component';
 import { UsuarioCooperativaComponent } from './usuario-cooperativa/usuario-cooperativa.component';
@@ -34,7 +33,6 @@ import { UsuarioCooperativaComponent } from './usuario-cooperativa/usuario-coope
     CarouselComponent,
     Parallax2Component,
     UsuarioEmpresaComponent,
-    UuarioCooperativaComponent,
     UsuarioClienteComponent,
     VoucherComponent,
     UsuarioCooperativaComponent
---
+++ frontEnd/src/app/service/cooperativa.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CooperativaService {
+
+  constructor(private http: HttpClient) { }
+
+  /*token = {
+    headers: new HttpHeaders().set('Authorization', )
+  }*/
+}
