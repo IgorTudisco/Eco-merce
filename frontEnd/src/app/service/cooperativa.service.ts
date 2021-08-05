9e13f939aea4c2d0de2dd531de7fe5caf4c41b79
Michele Diones <pereiradionesm@gmail.com>
2021-08-05 13:32:38 -0300

    Implentando cliente e cooperativa service

--- frontEnd/src/app/service/cooperativa.service.ts
+++ frontEnd/src/app/service/cooperativa.service.ts
@@ -1,14 +1,29 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Usuario } from '../model/Usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CooperativaService {
 
+
   constructor(private http: HttpClient) { }
 
-  /*token = {
-    headers: new HttpHeaders().set('Authorization', )
-  }*/
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getByIdCliente (id: number): Observable<Usuario>{
+    return this.http.get<Usuario>(`http://localhost:8080/usuario/id/${id}`, this.token)
+
+  }
+
+  getByEnderecoCliente (endereco: string): Observable<Usuario>{
+    return this.http.get<Usuario>(`http://localhost:8080/usuario/endereco/${endereco}`, this.token)
+
+  }
+
 }
